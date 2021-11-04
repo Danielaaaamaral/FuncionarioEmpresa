d80593aa8a5b3094428e2eea29f9a30551f8a15d
Danielaaaamaral <danielaaaamaral@gmail.com>
2021-11-03 23:40:28 -0300

    parte 4

--- EmpresaListarFuncionarios/Pages/funcionario/Delete.cshtml.cs
+++ EmpresaListarFuncionarios/Pages/funcionario/Delete.cshtml.cs
@@ -7,20 +7,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using EmpresaListarFuncionarios.Entidades.Classes;
 using EmpresaListarFuncionarios.Contexto;
+using EmpresaListarFuncionarios.Repositorio;
+using EmpresaListarFuncionarios.Entidades.Dtos;
 
 namespace EmpresaListarFuncionarios.Pages.funcionario
 {
     public class DeleteModel : PageModel
     {
         private readonly EmpresaFuncionarioContext _context;
+        private readonly IEmpresaFuncionarioRepositorio _repositorio;
 
-        public DeleteModel(EmpresaFuncionarioContext context)
+        public DeleteModel(EmpresaFuncionarioContext context, IEmpresaFuncionarioRepositorio repositorio)
         {
             _context = context;
+            _repositorio = repositorio;
         }
 
         [BindProperty]
         public Funcionario Funcionario { get; set; }
+        public FuncionarioResponseDto FuncionarioResponseDto { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -30,8 +35,13 @@ namespace EmpresaListarFuncionarios.Pages.funcionario
             }
 
             Funcionario = await _context.Funcionario.FirstOrDefaultAsync(m => m.IdFuncionario == id);
+            FuncionarioResponseDto = await _repositorio.BuscarFuncionarioResponsePorId((long)id);
 
             if (Funcionario == null)
+            {
+                return NotFound();
+            }
+            if (FuncionarioResponseDto == null)
             {
                 return NotFound();
             }
--- EmpresaListarFuncionarios/Pages/funcionario/Details.cshtml.cs
+++ EmpresaListarFuncionarios/Pages/funcionario/Details.cshtml.cs
@@ -7,19 +7,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using EmpresaListarFuncionarios.Entidades.Classes;
 using EmpresaListarFuncionarios.Contexto;
+using EmpresaListarFuncionarios.Entidades.Dtos;
+using EmpresaListarFuncionarios.Repositorio;
 
 namespace EmpresaListarFuncionarios.Pages.funcionario
 {
     public class DetailsModel : PageModel
     {
         private readonly EmpresaFuncionarioContext _context;
+        private readonly IEmpresaFuncionarioRepositorio _repositorio;
 
-        public DetailsModel(EmpresaFuncionarioContext context)
+        public DetailsModel(EmpresaFuncionarioContext context, IEmpresaFuncionarioRepositorio repositorio)
         {
             _context = context;
+            _repositorio = repositorio;
         }
 
         public Funcionario Funcionario { get; set; }
+        public FuncionarioResponseDto FuncionarioResponseDto { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -29,8 +34,14 @@ namespace EmpresaListarFuncionarios.Pages.funcionario
             }
 
             Funcionario = await _context.Funcionario.FirstOrDefaultAsync(m => m.IdFuncionario == id);
+            FuncionarioResponseDto = await _repositorio.BuscarFuncionarioResponsePorId((long) id);
 
             if (Funcionario == null)
+            {
+                return NotFound();
+            }
+
+            if (FuncionarioResponseDto == null)
             {
                 return NotFound();
             }
